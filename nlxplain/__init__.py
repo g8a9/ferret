e39f1c4773bd75c6f0ff4964efabb128ebde5a30
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2022-05-30 15:31:09 +0200

    refactor

--- nlxplain/__init__.py
+++ nlxplain/__init__.py
@@ -1,5 +1,7 @@
 """Top-level package for nlxplain."""
 
 __author__ = """Giuseppe Attanasio"""
-__email__ = 'giuseppeattanasio6@gmail.com'
-__version__ = '0.1.0'
+__email__ = "giuseppeattanasio6@gmail.com"
+__version__ = "0.1.0"
+
+from explainer import Explainer
