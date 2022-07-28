608281696c78b43b878bd8bdfaf4229f8e145bba
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2022-07-28 09:14:47 +0200

    bump version 0.2.1 -> 0.2.2

--- ferret/__init__.py
+++ ferret/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Giuseppe Attanasio"""
 __email__ = "giuseppeattanasio6@gmail.com"
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 
 from .explainers.shap import SHAPExplainer
--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/g8a9/ferret",
-    version="0.2.1",
+    version="0.2.2",
     zip_safe=False,
 )
