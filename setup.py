b436cbb85109d38e200bfe7a6e20f45517ed4f4f
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2023-04-13 01:22:35 +0200

    Bugfix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 from setuptools import find_packages, setup
 
-with open("README.rst") as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
 with open("HISTORY.rst") as history_file:
