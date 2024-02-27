67849e1778dbcbec77460cd0e2a9f13308f79a49
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2024-02-27 19:14:54 +0000

    Fix main inconsistencies after speech and task API integrations.
    * Update HISTORY to prepare for v0.5.0.
    * Deprecate two methods that run basic aggregations in Benchmark.
    * Update example notebooks according to the new APIs. Also slightly change the names.

--- HISTORY.rst
+++ HISTORY.rst
@@ -2,16 +2,24 @@
 Release notes
 =============
 
+0.5.0 (2024-02-27)
+------------------
+
+* [added] Task-API interface. (#35) The Benchmark class allows now to specify one of the supported NLP tasks and handles explanation and evaluation according to the semantic of the task.
+* [added] Support to speech models for classification (#36). The library exposes a new SpeechBenchmark class that implements the methodology presentend in `this paper <https://arxiv.org/abs/2309.07733>`_. 
+* [deprecated] We deprecated the methods *evaluate_samples* and *show_samples_evaluation_table* since they run basic aggregation / averaging which we decided to leave to the user.
+
+
 0.4.1 (2022-12-27)
 ------------------
 
-* Integrated interface to Thermostat datasets and pre-coumpute feature attributions
+* [added] Integrated interface to Thermostat datasets and pre-coumpute feature attributions
 
 0.4.0 (2022-09-01)
 ------------------
 
-* GPU inference for all the supported explainers 
-* Batched inference on both CPU and GPU (see our `usage guides <https://ferret.readthedocs.io/en/latest/usage.html>`_)
+* [added] GPU inference for all the supported explainers 
+* [added] Batched inference on both CPU and GPU (see our `usage guides <https://ferret.readthedocs.io/en/latest/usage.html>`_)
 * New cool-looking `docs <https://ferret.readthedocs.io/en/latest>`_ using `Furo <https://github.com/pradyunsg/furo>`_.
 
 0.1.0 (2022-05-30)
--- ferret/__init__.py
+++ ferret/__init__.py
@@ -4,6 +4,9 @@ __author__ = """Giuseppe Attanasio"""
 __email__ = "giuseppeattanasio6@gmail.com"
 __version__ = "0.4.1"
 
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 from .benchmark import Benchmark
 
@@ -33,21 +36,24 @@ from .explainers.lime import LIMEExplainer
 from .explainers.shap import SHAPExplainer
 from .modeling.text_helpers import TokenClassificationHelper
 
-# Speech-XAI section.
-from .benchmark_speech import SpeechBenchmark
 
-# Benchmarking methods
-from .evaluators.faithfulness_measures_speech import (
-    AOPC_Comprehensiveness_Evaluation_Speech,
-    AOPC_Sufficiency_Evaluation_Speech,
-)
+# Conditional imports for speech-related tasks 
+try:
+    # Explainers
+    from .explainers.explanation_speech.paraling_speech_explainer import ParalinguisticSpeechExplainer
+    from .explainers.explanation_speech.loo_speech_explainer import LOOSpeechExplainer
+    from .explainers.explanation_speech.explanation_speech import ExplanationSpeech
 
-# Explainers
-from .explainers.explanation_speech.paraling_speech_explainer import ParalinguisticSpeechExplainer
-from .explainers.explanation_speech.loo_speech_explainer import LOOSpeechExplainer
-from .explainers.explanation_speech.explanation_speech import ExplanationSpeech
+    # Model Helpers
+    from .modeling.speech_model_helpers.model_helper_er import ModelHelperER
+    from .modeling.speech_model_helpers.model_helper_fsc import ModelHelperFSC
+    from .modeling.speech_model_helpers.model_helper_italic import ModelHelperITALIC
+    from .benchmark_speech import SpeechBenchmark
 
-# Model Helpers
-from .modeling.speech_model_helpers.model_helper_er import ModelHelperER
-from .modeling.speech_model_helpers.model_helper_fsc import ModelHelperFSC
-from .modeling.speech_model_helpers.model_helper_italic import ModelHelperITALIC
+    # Benchmarking methods
+    from .evaluators.faithfulness_measures_speech import (
+        AOPC_Comprehensiveness_Evaluation_Speech,
+        AOPC_Sufficiency_Evaluation_Speech,
+    )
+except ImportError:
+    logger.info("Speech-related modules could not be imported. It is very likely that ferret was installed in the standard, text-only mode. Run `pip install ferret-xai[speech]` or `pip install ferret-xai[all] to include them.")
